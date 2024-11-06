21f126d047814421226f112e1969b76d7e417de4
Ricardo Tratch da Silva <engs-ricardodasilva@camporeal.edu.br>
2024-11-06 14:49:30 -0300

    Second Commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'create_acount.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _slideAnimation;
+  bool _isVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 800),
+      vsync: this,
+    );
+
+    _slideAnimation = Tween<Offset>(begin: const Offset(0, 0.5), end: Offset.zero)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _controller.forward();
+    _showComponentsWithDelay();
+  }
+
+  void _showComponentsWithDelay() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    setState(() {
+      _isVisible = true;
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,97 +48,122 @@ class LoginPage extends StatelessWidget {
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: const Text("Login - trocar por imagem"),
-        // title: Image(
-        //   image: NetworkImage("link"),
-        //   width: 140,
-        // ),
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-      backgroundColor: Color(0xFFFFC107),
+      backgroundColor: const Color(0xFF3498DB),
       body: Container(
         width: MediaQuery.of(context).size.width,
         padding: const EdgeInsets.all(27),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 30),
-            const Text(
-              "Login",
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-            const SizedBox(height: 30),
-            const CupertinoTextField(
-              padding: EdgeInsets.all(15),
-              placeholder: "Digite o seu email",
-              placeholderStyle: TextStyle(color: Colors.white70, fontSize: 14),
-              style: TextStyle(color: Colors.white70, fontSize: 14),
-              decoration: BoxDecoration(
-                color: Colors.deepOrange,
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-              ),
-            ),
-            const SizedBox(height: 5),
-            const CupertinoTextField(
-              padding: EdgeInsets.all(15),
-              placeholder: "Digite a sua senha",
-              obscureText: true,
-              placeholderStyle: TextStyle(color: Colors.white70, fontSize: 14),
-              style: TextStyle(color: Colors.white70, fontSize: 14),
-              decoration: BoxDecoration(
-                color: Colors.deepOrange,
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Align(
-              alignment: Alignment.center,
-              child: TextButton(
-                onPressed: () { },
+        child: SlideTransition(
+          position: _slideAnimation,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 30),
+              FadeTransition(
+                opacity: _controller,
                 child: const Text(
-                  "Esqueceu sua senha?",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: CupertinoButton(
-                padding: const EdgeInsets.all(17),
-                color: Colors.deepOrange,
-                child: const Text(
-                  "Acessar",
+                  "Login",
                   style: TextStyle(
-                    color: Colors.black38,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                onPressed: () {},
               ),
-            ),
-            const SizedBox(height: 7),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.white, width: 0.8),
-                borderRadius: BorderRadius.circular(15),
+              const SizedBox(height: 30),
+              AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: const Duration(milliseconds: 500),
+                child: CupertinoTextField(
+                  padding: const EdgeInsets.all(15),
+                  placeholder: "Digite o seu email",
+                  placeholderStyle: const TextStyle(color: Colors.black, fontSize: 14),
+                  style: const TextStyle(color: Colors.black, fontSize: 14),
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFF7F7F7),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                ),
               ),
-              child: TextButton(
-                child: const Text(
-                  "Crie sua Conta",
-                  style: TextStyle(
-                    color: Colors.white,
+
+              const SizedBox(height: 10),
+              AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: const Duration(milliseconds: 700),
+                child: CupertinoTextField(
+                  padding: const EdgeInsets.all(15),
+                  placeholder: "Digite a sua senha",
+                  obscureText: true,
+                  placeholderStyle: const TextStyle(color: Colors.black, fontSize: 14),
+                  style: const TextStyle(color: Colors.black, fontSize: 14),
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFF7F7F7),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 10),
+              Align(
+                alignment: Alignment.center,
+                child: AnimatedOpacity(
+                  opacity: _isVisible ? 1.0 : 0.0,
+                  duration: const Duration(milliseconds: 900),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Esqueceu sua senha?",
+                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600,),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+              AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: const Duration(milliseconds: 1000),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: CupertinoButton(
+                    padding: const EdgeInsets.all(17),
+                    color: Color(0xFF2ECC71),
+                    child: const Text(
+                      "Acessar",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
               
+              const SizedBox(height: 7),
+              AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: const Duration(milliseconds: 1200),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: TextButton(
+                    onPressed: () {
+                    },
+                    child: const Text(
+                      "Crie sua Conta",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
                 ),
-                onPressed: () {},
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
